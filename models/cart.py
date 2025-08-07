a7c016e889258d40d45aab6bdf1f784ab25416d7
Fariba <faribashakeri81@gmail.com>
2025-08-07 12:08:11 +0330

    cart_item modelis made

---
+++ models/cart.py
@@ -0,0 +1,31 @@
+from sqlalchemy import *
+from extentions import db
+from sqlalchemy.orm import backref
+
+class Cart(db.Model):
+    __tablename__ = "carts"
+    id = Column(Integer , primary_key=True)
+    status = Column(String , default="pending")
+    user_id =  Column(Integer , ForeignKey('user.id'), nullable=False)
+    user = db.relationship ('User' , backref = backref('carts', lazy = 'dynamic'))
+
+
+    def total_price(self):
+        total = 0
+        for item in self.cart_items:
+            t = item.price * item.quantity
+            total += t
+        return total
+    
+    def get_status_persian(self):
+        if self.status == 'pending':
+            return "در انتظار پرداخت"
+        
+        if self.status == 'paid':
+            return "پرداخت شده"
+
+        if self.status == 'sent':
+            return "ارسال شده"
+
+        if self.status == 'rejected':
+            return "رد شده"
