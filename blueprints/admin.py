2a15636cbdd576a329f627f7e8a62c883628416f
Fariba <faribashakeri81@gmail.com>
2025-08-07 13:02:08 +0330

    admin login bluprint added

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,8 +1,26 @@
-from flask import Blueprint,render_template
+from flask import Blueprint ,redirect,abort,render_template,session ,request
+import config
+
 
 app = Blueprint("admin" , __name__)
+
+@app.before_request
+def before_request():
+    if session.get('admin_login' , None) == None and request.endpoint != "admin_login":
+        abort(403)
 
 
 @app.route('/admin' , methods=["POST", "GET"])
 def login():
-    return render_template("admin/login.html")
+    if request.method == "POST":
+        username = request.form.get("username" , None)
+        password = request.form.get("password",None)
+
+        if username == config.ADMIN_USERNAME and password == config.ADMIN_PASSWORD:
+            session ['admin_login'] = username
+            return redirect("admin/dashboard")
+        else:
+            return redirect("admin")
+        
+    else:
+        return render_template("admin/login.html")
--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 SQLALCHEMY_DATABASE_URI = "sqlite:///fariba.db"
 SECRET_KEY = "fcdvfvduhujdcegydyuestgfcvhjsdgfyegsdgwdad;lldokfkjdjvbhbEE####%$%R^&@##@werfV"
+
+
+ADMIN_USERNAME = "fariba"
+ADMIN_PASSWORD = "1234"
